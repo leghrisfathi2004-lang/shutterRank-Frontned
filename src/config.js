6447d51b618748e1b295a9ce9673020c1335f0db
fathi leghris <leghrisfathi2004@gmail.com>
2026-09-24 03:58:45 +0000

    config: done

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 export const API_URL ='http://localhost:3000/api';
 export const TOKEN_KEY = 'sr_token';
+// backend marks a team "full" at this many players (team.service.js)
+export const TEAM_MAX_PLAYERS = 11;
