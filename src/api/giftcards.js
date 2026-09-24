258df55b85b447fb84ade37c4d8596d8cd95dfad
fathi leghris <leghrisfathi2004@gmail.com>
2026-09-24 03:58:00 +0000

    api: done

--- src/api/giftcards.js
+++ src/api/giftcards.js
@@ -1,21 +1,19 @@
 import { request, paginated } from './api.js';
 
 function listGiftcards (page) {
-  paginated('/giftcards', page);
+  return paginated('/giftcards', page);
 }
 
 function getGiftcard (id) {
-  request(`/giftcards/${id}`);
+  return request(`/giftcards/${id}`);
 }
 
 function createGiftcard ({ code, provider, value }) {
-  request('/giftcards/new', { method: 'POST', body: { code, provider, value } });
-
+  return request('/giftcards/new', { method: 'POST', body: { code, provider, value } });
 }
 
 function assignGiftcard (id, winnerId) {
-  request(`/giftcards/${id}/assign`, { method: 'PUT', body: { winnerId } });
-
+  return request(`/giftcards/${id}/assign`, { method: 'PUT', body: { winnerId } });
 }
 
-export {listGiftcards, getGiftcard, createGiftcard, assignGiftcard}
+export {listGiftcards, getGiftcard, createGiftcard, assignGiftcard};
